679e076300a9272794a8e9d36b7f964dc5008b7b
Guido Asbun <guido@asbun.io>
2021-06-25 00:53:34 -0700

    poseNet basics

--- script_posenet.js
+++ script_posenet.js
@@ -1,6 +1,7 @@
 let video;
 let poseNet;
 let pose;
+let skeleton;
 
 function setup() {
   createCanvas(640, 480);
@@ -14,6 +15,7 @@ function gotPoses(poses) {
   console.log(poses);
   if (poses.length > 0) {
     pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
   }
 }
 
@@ -25,7 +27,29 @@ function draw() {
   image(video, 0, 0);
 
   if (pose) {
+    let eyeR = pose.rightEye;
+    let eyeL = pose.leftEye;
+    let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+
     fill(255, 0, 0);
-    ellipse(pose.nose.x, psoe.nose.y, 64);
+    ellipse(pose.nose.x, pose.nose.y, d / 2);
+    fill(0, 0, 255);
+    ellipse(pose.rightWrist.x, pose.rightWrist.y, 10);
+    ellipse(pose.leftWrist.x, pose.leftWrist.y, 10);
+
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill(0, 255, 0);
+      ellipse(x, y, 16, 16);
+    }
+
+    for (let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
+      strokeWeight(2);
+      stroke(255)
+      line(a.position.x, a.position.y, b.position.x, b.position.y)
+    }
   }
 }
